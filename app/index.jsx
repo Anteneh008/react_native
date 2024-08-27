feaf845db840dfa08a5239e2e84139641bf0823a
Anteneh Atre <anteneh.atre@gmail.com>
2024-08-27 21:22:40 +0300

    about button

--- app/index.jsx
+++ app/index.jsx
@@ -1,35 +1,16 @@
-import { StatusBar } from "expo-status-bar";
 import {
-  StyleSheet,
-  Text,
-  View,
-  SafeAreaView,
+  Button,
   Platform,
   StatusBar as RNStatusBar,
-  Image,
-  TouchableWithoutFeedback,
-  TouchableOpacity,
-  TouchableHighlight,
-  TouchableNativeFeedback,
+  SafeAreaView,
+  StyleSheet,
 } from "react-native";
 
 export default function App() {
   const handlePress = () => console.log("Text clicked");
   return (
     <SafeAreaView style={styles.container}>
-      <Text numberOfLines={1} onPress={handlePress} style={styles.text}>
-        Hello React Native - A really really long text. Now I wanna make this
-        even longer and see what happens!
-      </Text>
-      <TouchableNativeFeedback onPress={() => console.log("Image tapped")}>
-        <View style={{width:200, height: 70, backgroundColor: "dodgerblue"}}></View>
-      </TouchableNativeFeedback>
-      <Image
-        source={require("../assets/icon.png")}
-        style={styles.image}
-        resizeMode="contain"
-      />
-      <StatusBar style="auto" />
+      <Button color="orange" title="Click Me" onPress={() => console.log("Button tapped")} />
     </SafeAreaView>
   );
 }
@@ -42,12 +23,5 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     paddingTop: Platform.OS === "android" ? RNStatusBar.currentHeight : 0,
     padding: 10,
-  },
-  text: {
-    marginBottom: 20, // Adds space between the text and image
-  },
-  image: {
-    width: 100, // Set width according to your needs
-    height: 100, // Set height according to your needs
   },
 });
