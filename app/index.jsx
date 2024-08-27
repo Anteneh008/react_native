f0966a75e4b3e0b4377a34b0935f60fac0ba876e
Anteneh Atre <anteneh.atre@gmail.com>
2024-08-27 21:32:19 +0300

    Alert

--- app/index.jsx
+++ app/index.jsx
@@ -4,14 +4,22 @@ import {
   StatusBar as RNStatusBar,
   SafeAreaView,
   StyleSheet,
-  Alert
+  Alert,
 } from "react-native";
 
 export default function App() {
-  
   return (
     <SafeAreaView style={styles.container}>
-      <Button color="orange" title="Click Me" onPress={() => alert("Button tapped")} />
+      <Button
+        color="orange"
+        title="Click Me"
+        onPress={() =>
+          Alert.alert("My title", "My message", [
+            { text: "Yes", onPress: () => console.log("Yes") },
+            { text: "No", onPress: () => console.log("No") },
+          ])
+        }
+      />
     </SafeAreaView>
   );
 }
