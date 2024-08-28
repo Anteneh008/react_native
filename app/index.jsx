aad130d7ef6a6a1aaee7cd5ce4021e400802c959
Anteneh Atre <anteneh.atre@gmail.com>
2024-08-28 21:19:33 +0300

    Building the view image screen

--- app/index.jsx
+++ app/index.jsx
@@ -2,9 +2,8 @@ import { useDeviceOrientation } from "@react-native-community/hooks";
 import React from "react";
 import { View } from "react-native";
 import WelcomeScreen from "./screens/WelcomeScreen";
+import ViewImageScreen from "./screens/ViewImageScreen";
 
 export default function App() {
-  return (
-    <WelcomeScreen />
-  );
+  return <ViewImageScreen />;
 }
