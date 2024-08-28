be012bfcb4104c7d33966feda0f814dc4735de8c
Anteneh Atre <anteneh.atre@gmail.com>
2024-08-28 19:06:12 +0300

    Absolute and Relative Positioning

--- app/index.jsx
+++ app/index.jsx
@@ -2,10 +2,6 @@ import { useDeviceOrientation } from "@react-native-community/hooks";
 import React from "react";
 import { View } from "react-native";
 
-// flexBasis equivalent to setting height and width property
-// flex: 1 amd flexGrow: 1 have the same result to fill the available space
-// flex: -1 and flexShrink are the same. if you have overflowing get shrink and other item fit on the screen
-
 export default function App() {
   return (
     <View
@@ -15,20 +11,32 @@ export default function App() {
         flexDirection: "row",
         justifyContent: "center",
         alignItems: "center",
-        alignContent: "center",
-        // flexWrap: "wrap",
       }}
     >
       <View
         style={{
           backgroundColor: "dodgerblue",
-          width: 400,
-          flex: -1,
+          width: 100,
           height: 100,
         }}
       />
-      <View style={{ backgroundColor: "gold", width: 100, height: 100 }} />
-      <View style={{ backgroundColor: "tomato", width: 100, height: 100 }} />
+      <View
+        style={{
+          backgroundColor: "gold",
+          width: 100,
+          height: 100,
+          top: 20,
+          left: 20,
+          position: "relative",
+        }}
+      />
+      <View
+        style={{
+          backgroundColor: "tomato",
+          width: 100,
+          height: 100,
+        }}
+      />
     </View>
   );
 }
