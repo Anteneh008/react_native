48675171b45306735213a317649015cbfe98a071
Anteneh Atre <anteneh.atre@gmail.com>
2024-08-28 20:56:21 +0300

    Building the welcome screen

--- app/index.jsx
+++ app/index.jsx
@@ -1,42 +1,10 @@
 import { useDeviceOrientation } from "@react-native-community/hooks";
 import React from "react";
 import { View } from "react-native";
+import WelcomeScreen from "./screens/WelcomeScreen";
 
 export default function App() {
   return (
-    <View
-      style={{
-        backgroundColor: "#fff",
-        flex: 1,
-        flexDirection: "row",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <View
-        style={{
-          backgroundColor: "dodgerblue",
-          width: 100,
-          height: 100,
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: "gold",
-          width: 100,
-          height: 100,
-          top: 20,
-          left: 20,
-          position: "relative",
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: "tomato",
-          width: 100,
-          height: 100,
-        }}
-      />
-    </View>
+    <WelcomeScreen />
   );
 }
