6672b6f8aeba4ec37216aa53860450c66b661ac7
Anteneh Atre <anteneh.atre@gmail.com>
2024-08-28 18:58:35 +0300

    flexBasis, flexgrow and flex shrink

--- app/index.jsx
+++ app/index.jsx
@@ -2,6 +2,10 @@ import { useDeviceOrientation } from "@react-native-community/hooks";
 import React from "react";
 import { View } from "react-native";
 
+// flexBasis equivalent to setting height and width property
+// flex: 1 amd flexGrow: 1 have the same result to fill the available space
+// flex: -1 and flexShrink are the same. if you have overflowing get shrink and other item fit on the screen
+
 export default function App() {
   return (
     <View
@@ -12,18 +16,19 @@ export default function App() {
         justifyContent: "center",
         alignItems: "center",
         alignContent: "center",
-        flexWrap: "wrap",
+        // flexWrap: "wrap",
       }}
     >
       <View
-        style={{ backgroundColor: "dodgerblue", width: 100, height: 100 }}
+        style={{
+          backgroundColor: "dodgerblue",
+          width: 400,
+          flex: -1,
+          height: 100,
+        }}
       />
       <View style={{ backgroundColor: "gold", width: 100, height: 100 }} />
       <View style={{ backgroundColor: "tomato", width: 100, height: 100 }} />
-      <View style={{ backgroundColor: "grey", width: 100, height: 100 }} />
-      <View
-        style={{ backgroundColor: "greenyellow", width: 100, height: 100 }}
-      />
     </View>
   );
 }
