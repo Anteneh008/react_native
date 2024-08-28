9833d28dd64d4fa2806dbeaca33ab25ff1a00b2c
Anteneh Atre <anteneh.atre@gmail.com>
2024-08-28 09:12:28 +0300

    flexbox-direction

--- app/index.jsx
+++ app/index.jsx
@@ -2,12 +2,17 @@ import { useDeviceOrientation } from "@react-native-community/hooks";
 import React from "react";
 import { View } from "react-native";
 
+
+// column, column-reverse, row, row-reverse 
+
 export default function App() {
   return (
-    <View style={{ backgroundColor: "#fff", flex: 1 }}>
-      <View style={{ backgroundColor: "dodgerblue", flex: 2 }} />
-      <View style={{ backgroundColor: "gold", flex: 1 }} />
-      <View style={{ backgroundColor: "tomato", flex: 1 }} />
+    <View style={{ backgroundColor: "#fff", flex: 1, flexDirection: "row" }}> 
+      <View
+        style={{ backgroundColor: "dodgerblue", width: 100, height: 100 }}
+      />
+      <View style={{ backgroundColor: "gold", width: 100, height: 100 }} />
+      <View style={{ backgroundColor: "tomato", width: 100, height: 100 }} />
     </View>
   );
 }
