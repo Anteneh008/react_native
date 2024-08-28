7f4ab2e785289075ab627952cd8de4f3c77c4f28
Anteneh Atre <anteneh.atre@gmail.com>
2024-08-28 09:31:46 +0300

    flexbox-justifyContent, alignItems and alignSelf

--- app/index.jsx
+++ app/index.jsx
@@ -2,14 +2,23 @@ import { useDeviceOrientation } from "@react-native-community/hooks";
 import React from "react";
 import { View } from "react-native";
 
+// justifyContent: "center", justifyContent: "flex-end", justifyContent: "flex-start", justifyContent: "space-around", justifyContent: "space-evenly", justifyContent: "space-between",
 
-// column, column-reverse, row, row-reverse 
+// alignItems: "center", alignItems: "baseline", alignItems: "baseline", alignItems: "flex-end", alignItems: "flex-start", alignItems: "stretch", alignSelf: "flex-start"
 
 export default function App() {
   return (
-    <View style={{ backgroundColor: "#fff", flex: 1, flexDirection: "row" }}> 
+    <View
+      style={{
+        backgroundColor: "#fff",
+        flex: 1,
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
       <View
-        style={{ backgroundColor: "dodgerblue", width: 100, height: 100 }}
+        style={{ backgroundColor: "dodgerblue", width: 100, height: 100, alignSelf: "flex-start" }}
       />
       <View style={{ backgroundColor: "gold", width: 100, height: 100 }} />
       <View style={{ backgroundColor: "tomato", width: 100, height: 100 }} />
