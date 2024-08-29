daece14bddd2a5b6c4e8086a6e047f6d25e135aa
Anteneh Atre <anteneh.atre@gmail.com>
2024-08-29 09:32:25 +0300

    platform specific code

--- app/components/AppText.js
+++ app/components/AppText.js
@@ -1,15 +1,25 @@
 import React from "react";
 import { Platform, StyleSheet, Text } from "react-native";
 
-
 const AppText = ({ children }) => {
   return <Text style={styles.text}>{children}</Text>;
 };
 
+
+
 const styles = StyleSheet.create({
   text: {
-    fontSize: 18,
-    fontFamily: Platform.OS === "android" ? "Roboto" : "Avenire",
+    color: "tomato",
+    ...Platform.select({
+        ios: {
+          fontSize: 20,
+          fontFamily: "Avenir",
+        },
+        android: {
+          fontSize: 18,
+          fontFamily: "Roboto",
+        },
+      })
   },
 });
 
