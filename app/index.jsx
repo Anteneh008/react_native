1acd9d89d8e4356ca89e287fa1e9c1b18138c47c
Anteneh Atre <anteneh.atre@gmail.com>
2024-08-29 07:33:01 +0300

    border

--- app/index.jsx
+++ app/index.jsx
@@ -1,9 +1,27 @@
-import { useDeviceOrientation } from "@react-native-community/hooks";
 import React from "react";
 import { View } from "react-native";
-import WelcomeScreen from "./screens/WelcomeScreen";
-import ViewImageScreen from "./screens/ViewImageScreen";
 
 export default function App() {
-  return <ViewImageScreen />;
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <View
+        style={{
+          backgroundColor: "dodgerblue",
+          width: 100,
+          height: 100,
+          borderWidth: 10,
+          borderColor: "royalblue",
+          borderRadius: 50,
+          // borderTopWidth: 20,
+          // borderTopLeftRadius: 50,
+        }}
+      ></View>
+    </View>
+  );
 }
