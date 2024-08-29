50d9a7592b4392ac6c5ccb69717ae5955f60f9cd
Anteneh Atre <anteneh.atre@gmail.com>
2024-08-29 09:47:08 +0300

    Organizing styles

--- app/index.jsx
+++ app/index.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { View } from "react-native";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
 import AppText from "./components/AppText";
 
 // https://icons.expo.fyi/Index
