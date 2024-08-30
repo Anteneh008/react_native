18d54fa4eaba4e9b1769bb53a3192c0e68f662c2
Anteneh Atre <anteneh.atre@gmail.com>
2024-08-30 05:42:45 +0300

    Building thr card component

--- app/components/AppText/styles.js
+++ app/components/AppText/styles.js
@@ -2,7 +2,7 @@ import { Platform, StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
   text: {
-    color: "tomato",
+    color: "black",
     ...Platform.select({
       ios: {
         fontSize: 20,
--- app/index.jsx
+++ app/index.jsx
@@ -1,12 +1,19 @@
 import React from "react";
-import { TouchableOpacity, View } from "react-native";
-import AppButton from "./components/AppButton";
-import WelcomeScreen from "./screens/WelcomeScreen";
+import { View } from "react-native";
+import Card from "./components/Card";
 
 // https://icons.expo.fyi/Index
 
 export default function App() {
   return (
-    <WelcomeScreen />
+    <View
+      style={{
+        backgroundColor: "#f8f4f4",
+        padding: 20,
+        paddingTop: 100,
+      }}
+    >
+      <Card title="Redjacket for sale!" price="$100" image={require("./assets/jacket.jpg")}/>
+    </View>
   );
 }
