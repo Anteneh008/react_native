c2f3c8ae9f74afca66fce9140811612f1194e854
Anteneh Atre <anteneh.atre@gmail.com>
2024-08-30 06:01:56 +0300

    Building the Listing Details

--- app/index.jsx
+++ app/index.jsx
@@ -1,19 +1,10 @@
 import React from "react";
 import { View } from "react-native";
 import Card from "./components/Card";
+import ListingDetailsScreen from "./screens/ListingDetailsScreen";
 
 // https://icons.expo.fyi/Index
 
 export default function App() {
-  return (
-    <View
-      style={{
-        backgroundColor: "#f8f4f4",
-        padding: 20,
-        paddingTop: 100,
-      }}
-    >
-      <Card title="Redjacket for sale!" price="$100" image={require("./assets/jacket.jpg")}/>
-    </View>
-  );
+  return <ListingDetailsScreen />;
 }
