9007beadf927a9d496b018dff59baaa8af5f10b2
Anteneh Atre <anteneh.atre@gmail.com>
2024-09-01 07:18:30 +0300

    TextInput

--- app/index.jsx
+++ app/index.jsx
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import ListingsScreen from "./screens/ListingsScreen";
+import Screen from "./components/Screen";
+import { Text, TextInput } from "react-native";
 
 export default function App() {
+  const [firstName, setFirstName] = useState("");
   return (
-    <ListingsScreen />
+    <Screen>
+      <Text></Text>
+      <TextInput
+        secureTextEntry={true}
+        maxLength={10}
+        keyboardType="numeric"
+        clearButtonMode="always" // only work on IOS
+        onChangeText={(text) => setFirstName(text)}
+        placeholder="First Name"
+        style={{ borderBottomColor: "#ccc", borderBottomWidth: 1 }}
+      />
+    </Screen>
   );
 }
